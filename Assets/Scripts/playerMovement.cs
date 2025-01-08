2aa7a0ce4a1c27de60076fe6f3d991e75e67ee9b
OmriTafla <omritafla@gmail.com>
2025-01-08 15:38:37 +0200

    player animation functional including axe

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -6,15 +6,13 @@ public class playerMovement : MonoBehaviour
 {
     [SerializeField] float speed = 1f;
     [SerializeField] float rotationSpeed = 1f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] private Animator animator;
+    private int movementState = 0; //to control the animation
+    
     // Update is called once per frame
     void Update()
     {
+        //movement
         if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(0,0,speed*Time.deltaTime);
@@ -38,5 +36,20 @@ public class playerMovement : MonoBehaviour
                 transform.Translate(0, 0, 2 * speed * Time.deltaTime);
             }
         }
+        
+        //animation
+        movementState = 0;
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                movementState = 2;
+            }
+            else
+            {
+                movementState = 1;
+            }
+        }
+        animator.SetInteger("movementState", movementState);
     }
 }
