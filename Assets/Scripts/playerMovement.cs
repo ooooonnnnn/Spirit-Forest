ef625369545e6e7b27ae771ed8731e68004623ac
miladonn <miladon1173@gmail.com>
2025-01-09 15:14:42 +0200

    Added a new movement script - moving left and right

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class playerMovement : MonoBehaviour
@@ -8,48 +9,71 @@ public class playerMovement : MonoBehaviour
     [SerializeField] float rotationSpeed = 1f;
     [SerializeField] private Animator animator;
     private int movementState = 0; //to control the animation
-    
+    private int startingPosition = 0; // if the starting position is 0, means the character is in the middle
+    [SerializeField] float distanceOfSwipe = 2f;
+    private float locationOfPlayerXaxis;
+    private float locationOfPlayerYaxis;
+    private float locationOfPlayerZaxis;
+
+
+    private void Start()
+    {
+        startingPosition = 0;
+    }
     // Update is called once per frame
     void Update()
     {
-        //movement
-        if (Input.GetKey(KeyCode.W))
+        Debug.Log(startingPosition);
+        locationOfPlayerXaxis = transform.position.x;
+        locationOfPlayerYaxis = transform.position.y;
+        locationOfPlayerZaxis = transform.position.z;
+
+        transform.Translate(0, 0, speed * Time.deltaTime); // makes the player move forward
+
+
+        if (Input.GetKeyDown(KeyCode.D))
         {
-            transform.Translate(0,0,speed*Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(0, 0, -speed * Time.deltaTime);
-        }
-        if(Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(0, rotationSpeed *Time.deltaTime,0);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(0,-rotationSpeed * Time.deltaTime ,0);
-        }
-        if(Input.GetKey(KeyCode.LeftShift))
-        {
-            if(Input.GetKey(KeyCode.W))
+            if (startingPosition == 0 || startingPosition == -1)
             {
-                transform.Translate(0, 0, 2 * speed * Time.deltaTime);
+                transform.position = new Vector3((float)locationOfPlayerXaxis + distanceOfSwipe, locationOfPlayerYaxis, locationOfPlayerZaxis); // go right
+
             }
+            if (startingPosition == 0)
+            {
+                startingPosition++;
+            }
+            else if (startingPosition == -1)
+            {
+                startingPosition++;
+            }
+            Debug.Log(startingPosition);
+
         }
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (startingPosition == 0 || startingPosition == 1)
+            {
+                transform.position = new Vector3((float)(locationOfPlayerXaxis - distanceOfSwipe), locationOfPlayerYaxis, locationOfPlayerZaxis); // go left
+
+            }
+            if (startingPosition == 0)
+            {
+                startingPosition--;// -1 position means the character is on the left lane
+            }
+            else if (startingPosition == 1)
+            {
+                startingPosition--;
+            }
+
+        }
+
         //animation
-        movementState = 0;
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
-        {
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                movementState = 2;
-            }
-            else
-            {
-                movementState = 1;
-            }
-        }
+        movementState = 2;
+
+
         animator.SetInteger("movementState", movementState);
     }
+
+
 }
+
