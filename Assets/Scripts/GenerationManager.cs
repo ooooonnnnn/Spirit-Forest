73fbbc03228903a457b6727a041373ff421b2ed1
OmriTafla <omritafla@gmail.com>
2025-01-10 16:36:12 +0200

    Create consecutive trail sections

--- Assets/Scripts/GenerationManager.cs
+++ Assets/Scripts/GenerationManager.cs
@@ -1,25 +1,79 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.Splines;
+using Random = UnityEngine.Random;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class GenerationManager : MonoBehaviour
 {
     // Dynamically creates the level
-	[SerializeField] private SplineContainer splineContainer;
-	[SerializeField] private MeshFilter meshFilter;
-	private Mesh mesh;
+    [SerializeField] private GameObject sectionPrefab;
+    [SerializeField] private Transform trail;
+    [SerializeField] private int numSections;
+    private List<GameObject> trailSections = new();
+
+	[Header("Spline Parameters")] [SerializeField]
+	private int numAnchors;
+	[SerializeField] private float distanceBetweenAnchors;
+	[SerializeField] private float angleRange;
+	
 	
 	public void Start()
 	{
-		mesh = new Mesh();
+		//create an initial run of trail sections
+		Vector3 origin = Vector3.zero;
+		Vector3 direction = Vector3.forward;
+		for (int i = 0; i < numSections; i++)
+		{
+			GameObject newSection = Instantiate(sectionPrefab, trail);
+			trailSections.Add(newSection);
+			SplineContainer container = newSection.GetComponent<SplineContainer>();
+			MeshFilter meshFilter = newSection.GetComponent<MeshFilter>();
+			container.Spline = i==0 ? NewSpline(origin, direction) : NewSpline(trailSections[i-1].GetComponent<SplineContainer>());
+			SetMeshFromSpline(container, meshFilter);
+		}
+	}
+
+	private Spline NewSpline(SplineContainer oldSpline)
+	{
+		//makes a new SplineContainer that is tangent and congruent with another spline oldSpline
+		BezierKnot lastAnchor = oldSpline.Spline.Last();
+		return NewSpline(lastAnchor.Position, Vector3.Normalize(lastAnchor.TangentOut));
+	}
+
+	private Spline NewSpline(Vector3 origin, Vector3 direction)
+	{
+		//makes a new SplineContainer with a spline that starts in origin and initially faces towards direction
+		Spline spline = new Spline();
+		for (int i = 0; i < numAnchors; i++)
+		{
+			spline.Add(new BezierKnot(origin, -direction, direction));
+			
+			float dirChange = (Random.value - 0.5f) * angleRange * 2;
+			print(dirChange);
+			direction = UnitVectorByAngle(Vector3.up, direction, dirChange);
+			origin += distanceBetweenAnchors * direction;
+		}
+		return spline;
+	}
+	
+	private void SetMeshFromSpline(SplineContainer splineContainer, MeshFilter meshFilter)
+	{
+		//sets the mesh in meshFilter as a snake with radius 1 around the spline in splineContainer
 		
-		int numCenters = 20;
+		Mesh mesh = new Mesh();
+		
+		int numCenters = 50;
 		float[] positionsOnSpline = new float[numCenters];
-		for (int i = 0; i < numCenters; i++) positionsOnSpline[i] = i * 1f / numCenters;
+		for (int i = 0; i < numCenters; i++) positionsOnSpline[i] = i * 1f / (numCenters - 1);
 		float3[] centers = new float3[numCenters];
 		float3[] forwards = new float3[numCenters];
 
@@ -31,14 +85,12 @@ public class GenerationManager : MonoBehaviour
 		
 		for (int i = 0; i < numCenters; i++)
 		{
-			float3 dummy;
-			splineContainer.Evaluate(positionsOnSpline[i], out centers[i], out forwards[i], out dummy);
+			splineContainer.Evaluate(positionsOnSpline[i], out centers[i], out forwards[i], out _);
 
 			for (int j = 0; j < numVertsPerCenter; j++)
 			{
 				float angle = j * (2 * (float)Math.PI) / numVertsPerCenter;
-				Vector3 side = Vector3.Cross(Vector3.up,  Vector3.Normalize(forwards[i]));
-				Vector3 ray = Vector3.up * (float)Math.Cos(angle) + side * (float)Math.Sin(angle);
+				Vector3 ray = UnitVectorByAngle(forwards[i], Vector3.up, angle);
 
 				int vertInd = i * numVertsPerCenter + j;
 				vertices[vertInd] = (Vector3)centers[i] + ray;
@@ -72,13 +124,13 @@ public class GenerationManager : MonoBehaviour
 		meshFilter.mesh = mesh;
 	}
 
-	
-	//Test
-	private int index = 0;
-	public void Update()
+	private Vector3 UnitVectorByAngle(Vector3 axis, Vector3 forward, float angle)
 	{
-		// if (index >= mesh.vertices.Length) index = 0;
-		// Debug.DrawLine(mesh.vertices[index], mesh.vertices[index] + 0.1f * Vector3.up, Color.white, 0.2f);
-		// index++;
+		//returns a vector that is forward rotated by angle radian about up
+		axis = axis.normalized;
+		forward = forward.normalized;
+		Vector3 side = -Vector3.Cross(axis, forward);
+
+		return forward * (float)Math.Cos(angle) + side * (float)Math.Sin(angle);
 	}
 }
