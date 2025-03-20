4033400028bd57706c16fb4c4e73bd67c0ee1ba0
OmriTafla <omritafla@gmail.com>
2025-03-20 10:14:56 +0200

    .

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,54 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
 public class GameManager : MonoBehaviour
 {
-    //
+    [Header("Player variables")]
     [SerializeField] private int health;
     [SerializeField] private PlayerMovement playerMovement;
+    [SerializeField] private Animator playerAnimator;
     
+    [Header("UI Screens")] 
+    [SerializeField] private GameObject ingameInfoCanvas;
+    [SerializeField] private TMP_Text healthText;
+    [SerializeField] private GameObject deathScreen;
+    [SerializeField] private TMP_Text deathScreenTotalSouls;
+
+    private void Start()
+    {
+        ingameInfoCanvas.SetActive(true);
+        deathScreen.SetActive(false);
+        healthText.text = "Health: " + health;
+    }
+
     public void OnObstacleHit()
     {
         health--;
-        print(health <= 0 ? "dead" : $"ouch, health is {health}");
-        playerMovement.SlowDownOnHit();
+        if (health > 0)
+        {
+            healthText.text = "Health: " + health;
+            playerMovement.SlowDownOnHit();
+        }
+        else
+        {
+            playerMovement.speed = 0;
+            playerMovement.enabled = false;
+            playerAnimator.enabled = false;
+            
+            ingameInfoCanvas.SetActive(false);
+            deathScreen.SetActive(true);
+            deathScreenTotalSouls.text = $"Collected {ScoreManager.inst.score} Souls";
+        }
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene("ProceduralLevel");
     }
 }
